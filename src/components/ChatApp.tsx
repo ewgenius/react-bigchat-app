9aa498496630b98e3582bcf70d905d46ed518818
ewgenius <ewgeniux@gmail.com>
2016-09-18 12:14:19 +0300

    upd

--- src/components/ChatApp.tsx
+++ src/components/ChatApp.tsx
@@ -3,6 +3,7 @@ import {Component} from 'react'
 import {range} from 'ramda'
 
 import AppBar from 'material-ui/AppBar'
+import Avatar from 'material-ui/Avatar'
 import ListItem from 'material-ui/List/ListItem'
 import Flex from './Layout/Flex.tsx'
 import Scroller from './Layout/Scroller.tsx'
@@ -36,7 +37,10 @@ export default class ChatApp extends Component<{}, ChatAppState> {
         title='react big chat'
         titleStyle={{ fontSize: 20, textTransform: 'uppercase' }}/>
       <Scroller vertical>
-        {clients.map((client, i) => <ListItem key={i} primaryText={client.name}/>) }
+        {clients.map((client, i) => <ListItem
+          leftAvatar={<Avatar src={`https://robohash.org/set_set3/bgset_bg1/${i}?size=256x256`} />}
+          key={i} primaryText={client.name}
+          />) }
       </Scroller>
     </Flex>
   }
